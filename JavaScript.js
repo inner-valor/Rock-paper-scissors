b9d0e02df69ec480cddd94b88aecefccbcc8fd77
inner-valor <ihsaan5555@gmail.com>
2022-04-18 18:08:07 -0500

    add round, condition, and game functions. fix scissors choice

--- JavaScript.js
+++ JavaScript.js
@@ -1,50 +1,85 @@
 
-/*
-com will randomly pick rock, paper, or scissor.
-
-player will pick rock, paper, or scissor.
-
-a function compiers the com and the players picks:
-paper beats rock, rock beats scissor, and scissor beats paper.
-
-*/
-
-/*
-OK. com function will randomly pick 1 to 3.
-OK. 1 will be rock and 3 will be scissors.
-*/
 const min = 1;
 const max = 3;
 
-let com;
-let player;
 
+// create a random number.
 function randomNumber() {
    return Math.floor(Math.random() * (max - min + 1) + min);
     
 }
 
+// choses a random number 1 - 3 to pick between rock and scissors.
 function computerPlay() {
-
-
+    
     if (randomNumber() === 1) {
-       return com = "rock";
+       return String("rock");
 
     } else if (randomNumber() === 2) {
-       return com = "paper";
+       return String("paper");
 
     } else {
-       return com = "scissor";
+       return String("scissor");
 
     }
     
 }
+// records the wins, loss, and rounds.
+let win = 0;
+let loss = 0;
+let r = 0;
+
+// game mechanics.
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection.toLowerCase() == "rock" && computerSelection === "scissor"|| 
+        playerSelection.toLowerCase() == "paper" && computerSelection === "rock"||
+        (playerSelection.toLowerCase() == "scissor" || playerSelection.toLowerCase() == "scissors") && computerSelection === "paper") 
+    {
+        return win++, console.log(`you Won round ${r + 1}! ${playerSelection} beats ${computerSelection}`);
+        
+        
+    } else if (playerSelection.toLowerCase() == computerSelection || playerSelection.toLowerCase() == "scissors" && computerSelection == "scissor") {
+        return console.log(`no-one wins round ${r + 1}`);
+
+    } else {
+        return loss++, console.log(`you loss round ${r + 1}! ${computerSelection} beats ${playerSelection}`);
+        
+    }
+    
+}
+// win/loss conditions
+function condition(r) {
+    if (win > loss && r === 5) {
+        return console.log("WINNER!!");
+
+    } else if (win < loss && r === 5) {
+        return console.log("LOSER!!");
+        
+    } else if (win == loss && r === 5) {
+        return console.log("Tie");
+    }
+}
+let playerSelection;
+
+// game rounds
+function game() {
+    
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("rock paper or scissors");
+
+        playRound(playerSelection, computerPlay())
+        
+        r++;
+    }
+
+    condition(r)
+// reset game score.
+    r = 0;
+    win = 0;
+    loss = 0;
 
 
-
-
-
-
-
+}
 
 
